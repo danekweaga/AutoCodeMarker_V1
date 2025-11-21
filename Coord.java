0d2f64becf5d9d2a9c293c87a4a391542f575bbf
Aniekan Ekarika <ani8dar@gmail.com>
2025-11-21 19:32:23 -0400

    added coord class

---
+++ Coord.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Coord here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Coord
+{
+    // run tests
+    
+    // manage test suites
+    
+    // manage test cases
+}
