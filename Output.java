0bf3b5559d5506933bc1e2ab151d85659839dd5e
Chukwunonso Daniel Ekweaga <88860622+danekweaga@users.noreply.github.com>
2025-11-22 04:46:47 -0400

    Created Output.java file

---
+++ Output.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class TestCase here.
+ *
+ * @author Alamin Adeleke, Chuckwunonso Ekweaga, Aniekan Ekarika, Frances Felicidario
+ * @version (a version number or a date)
+ */
+public class Ouput
+{
+    //Instance data
+    Arraylist<Result> result;
+
+    public void addResult(Result result)
+    {
+        result.add(result);
+    }
+    
+}
