1c58134f0909db7b63256e9118e6813895c6b8e1
Chukwunonso Daniel Ekweaga <88860622+danekweaga@users.noreply.github.com>
2025-11-22 05:10:13 -0400

    Added minimal getters and setters method

--- Result.java
+++ Result.java
@@ -1,7 +1,42 @@
- public class Result
- {
-        //Instance data 
-        String testCaseName;
-        String result;
+public class Result {
+       // Instance data
+       private String testCaseName;
+       private String result;
 
- }
+       // No-arg constructor
+       public Result() { }
+
+       // Convenience constructor
+       public Result(String testCaseName, String result) 
+       {
+              this.testCaseName = testCaseName;
+              this.result = result;
+       }
+
+       // Getters and setters
+       public String getTestCaseName()
+       {
+              return testCaseName;
+       }
+
+       public void setTestCaseName(String testCaseName) 
+       {
+              this.testCaseName = testCaseName;
+       }
+
+       public String getResult() {
+              return result;
+       }
+
+       public void setResult(String result) 
+       {
+              this.result = result;
+       }
+
+       @Override
+       public String toString() 
+       {
+              return "Result{testCaseName='" + testCaseName + "', result='" + result + "'}";
+       }
+
+}
