aefd9791a2e6a78b86bd74c6304f52f11ea7727e
skye / frances <75171465+shigasaki@users.noreply.github.com>
2025-11-22 16:45:59 -0400

    Update TestSuite.java
    Refine TestSuite with generics, name accessors, clearTestCases, and documented executeOnProgram

--- TestSuite.java
+++ TestSuite.java
@@ -1,33 +1,95 @@
 
 /**
- * Test Suite containing multiple TestCase objects. This stores and manages set of test cases belonging to this suite.
+ * @title    Test Suite containing multiple TestCase objects. This stores and manages set of test cases belonging to this suite.
  *
- * @author Frances Felicidario, Alamin Adeleke, Aniekan Ekarika, Chukwunonso Ekweaga
+ * @author   Frances Felicidario, Alamin Adeleke, Aniekan Ekarika, Chukwunonso Ekweaga
+ *
  * @version 11-22-25
  */
-public class TestSuite 
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TestSuite
 {
+    // Instance data
     private String name;
     private final List<TestCase> testCases = new ArrayList<>();
 
-    public TestSuite(String name) 
+    /***********************************************************************************
+     * Constructs a TestSuite with the given name.
+     *
+     * @param name the name of this test suite
+     ***********************************************************************************/
+    public TestSuite(String name)
     {
         this.name = name;
     }
 
-    public void addTestCase(TestCase testCase) 
+    /***********************************************************************************
+     * Gets the name of this TestSuite.
+     *
+     * @return the test suite name
+     ***********************************************************************************/
+    public String getName()
+    {
+        return name;
+    }
+
+    /***********************************************************************************
+     * Sets the name of this TestSuite.
+     *
+     * @param name the new suite name
+     ***********************************************************************************/
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    /***********************************************************************************
+     * Adds a TestCase to this TestSuite.
+     *
+     * @param testCase the TestCase to add
+     ***********************************************************************************/
+    public void addTestCase(TestCase testCase)
     {
         testCases.add(testCase);
     }
 
-    public List<TestCase> getTestCases() 
+    /***********************************************************************************
+     * Removes all TestCase objects from this TestSuite.
+     ***********************************************************************************/
+    public void clearTestCases()
+    {
+        testCases.clear();
+    }
+
+    /***********************************************************************************
+     * Returns an unmodifiable view of the test cases in this suite.
+     *
+     * @return list of TestCase objects
+     ***********************************************************************************/
+    public List<TestCase> getTestCases()
     {
         return Collections.unmodifiableList(testCases);
     }
 
-    public void executeOnProgram(Program program) throws IOException, InterruptedException 
+    /***********************************************************************************
+     * Executes all test cases in this suite on the given Program.
+     *
+     * Each TestCase's actualOutput and passed fields are updated based on the
+     * Program's output.
+     *
+     * @param program the Program to execute these test cases on
+     * @throws IOException          if compilation or execution fails
+     * @throws InterruptedException if the execution is interrupted
+     ***********************************************************************************/
+    public void executeOnProgram(Program program)
+        throws IOException, InterruptedException
     {
-        for (TestCase tc : testCases) 
+        for (TestCase tc : testCases)
         {
             String output = program.runWithInput(tc.getInput());
             tc.setActualOutput(output);
