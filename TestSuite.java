4806f56b2ed2bef13a3e0871e8d09478bf3c3835
skye / frances <75171465+shigasaki@users.noreply.github.com>
2025-11-22 15:57:24 -0400

    Updated TestSuite.java
    Previously basically empty. Implemented TestSuite properly such that it knows its test cases and can execute them on a given program

--- TestSuite.java
+++ TestSuite.java
@@ -1,11 +1,38 @@
 
 /**
- * Write a description of class TestSuite here.
+ * Test Suite containing multiple TestCase objects. This stores and manages set of test cases belonging to this suite.
  *
- * @author Alamin Adeleke, Aniekan Ekarika, Chuckwunonso Ekweaga
- * @version (a version number or a date)
+ * @author Frances Felicidario, Alamin Adeleke, Aniekan Ekarika, Chuckwunonso Ekweaga
+ * @version 11-22-25
  */
-public class TestSuite
+public class TestSuite 
 {
-    
+    private String name;
+    private final List<TestCase> testCases = new ArrayList<>();
+
+    public TestSuite(String name) 
+    {
+        this.name = name;
+    }
+
+    public void addTestCase(TestCase testCase) 
+    {
+        testCases.add(testCase);
+    }
+
+    public List<TestCase> getTestCases() 
+    {
+        return Collections.unmodifiableList(testCases);
+    }
+
+    public void executeOnProgram(Program program) throws IOException, InterruptedException 
+    {
+        for (TestCase tc : testCases) 
+        {
+            String output = program.runWithInput(tc.getInput());
+            tc.setActualOutput(output);
+            tc.setPassed(tc.getExpectedOutput().trim().equals(output.trim()));
+        }
+    }
 }
+
