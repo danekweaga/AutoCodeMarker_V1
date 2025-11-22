2691e505143e1ba89314764571d7ee3e7ececadc
Chukwunonso Daniel Ekweaga <88860622+danekweaga@users.noreply.github.com>
2025-11-22 04:39:27 -0400

    Changed parameters forfor runtests method

--- Coord.java
+++ Coord.java
@@ -7,43 +7,54 @@
 import javafx.stage.Stage;
 import javafx.stage.Modality;
 
-public class Coord {
+public class Coord 
+{
     private final Stage owner;
 
     private TestSuiteManager suiteManager;
     private TestCaseManager caseManager;
 
-    public Coord(Stage owner) {
+    public Coord(Stage owner) 
+    {
         this.owner = owner;
     }
 
     // run tests
-    public void runTests(String submissionFolder, String testSuiteFolder) {
+    public void runTests(string Submission Folder, string testsuiteFolder) 
+    {
         System.out.println("Run test button clicked");
     }
 
     // manage test suites
-    public void manageTestSuites() {
-        if (suiteManager == null || !suiteManager.isShowing()) {
+    public void manageTestSuites() 
+    {
+        if (suiteManager == null || !suiteManager.isShowing()) 
+            {
             suiteManager = new TestSuiteManager();
             suiteManager.initOwner(owner);
             suiteManager.initModality(Modality.NONE);
             suiteManager.setOnCloseRequest(e -> suiteManager = null); // reset on close
             suiteManager.show();
-        } else {
+        } 
+        else 
+            {
             suiteManager.toFront();
         }
     }
 
     // manage test cases
-    public void manageTestCases() {
-        if (caseManager == null || !caseManager.isShowing()) {
+    public void manageTestCases() 
+    {
+        if (caseManager == null || !caseManager.isShowing()) 
+            {
             caseManager = new TestCaseManager();
             caseManager.initOwner(owner);
             caseManager.initModality(Modality.NONE);
             caseManager.setOnCloseRequest(e -> caseManager = null); // reset on close
             caseManager.show();
-        } else {
+        } 
+        else
+        {
             caseManager.toFront();
         }
     }
