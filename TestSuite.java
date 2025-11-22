cef937f974e556f897ba7decc4f59b058ba7553e
Chukwunonso Daniel Ekweaga <88860622+danekweaga@users.noreply.github.com>
2025-11-22 16:02:57 -0400

    name change

--- TestSuite.java
+++ TestSuite.java
@@ -2,7 +2,7 @@
 /**
  * Test Suite containing multiple TestCase objects. This stores and manages set of test cases belonging to this suite.
  *
- * @author Frances Felicidario, Alamin Adeleke, Aniekan Ekarika, Chuckwunonso Ekweaga
+ * @author Frances Felicidario, Alamin Adeleke, Aniekan Ekarika, Chukwunonso Ekweaga
  * @version 11-22-25
  */
 public class TestSuite 
