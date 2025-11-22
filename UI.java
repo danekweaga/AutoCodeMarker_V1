275f22121a02bf04dba6f14673e6b196237c5ed6
Alamin Adeleke <alaminadeleke5@gmail.com>
2025-11-21 20:09:48 -0400

    Create UI.java

---
+++ UI.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/***************************************************************************************
+ * Write a description of class UI here.
+ *
+ * @author Alamin Adeleke, Chuckwunonso Ekweaga, Aniekan Ekarika, Frances Felicidario
+ * @version 1
+ ***************************************************************************************/
+public class UI extends Application
+{
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) 
+    {
+        
+    }
+}
