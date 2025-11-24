2e3af15fe4cc446bbf4314c652fcc771efcbed4b
Aniekan Ekarika <ani8dar@gmail.com>
2025-11-24 12:34:24 -0400

    added test submissions

---
+++ test_submissions/complie error, missing brace/AddSyntaxError.java
@@ -0,0 +1,4 @@
+public class AddSyntaxError {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    // missing closing brace
---
+++ test_submissions/correct double/AddDouble.java
@@ -0,0 +1,5 @@
+public class AddDouble {
+    public static void main(String[] args) {
+        System.out.println(Double.parseDouble(args[0]) + Double.parseDouble(args[1]));
+    }
+}
